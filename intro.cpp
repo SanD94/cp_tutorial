a0f37844b895da515b79c3095604445b29869d6a
Safa Andac <8544658+SanD94@users.noreply.github.com>
2024-10-04 11:47:05 +0200

    finish intro

--- intro.cpp
+++ intro.cpp
@@ -6,6 +6,25 @@
 // g++ -std=c++11 -O2 -Wall filename.cpp -o filename
 
 // code shortening
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair 
+#define REP(i, a, b) for (int i = (a); i <= (b); i++)
+#define SQ(a) (a)*(a)
+
+// example:
+// v.push_back(make_pair(y1, x1));
+// v.push_back(make_pair(y2, x2));
+// int d = v[i].first + v[i].second
+//
+// v.PB(MP(y1, x1));
+// v.PB(MP(y2, x2));
+// int d = v[i].F + v[i].S;
 
 using namespace std;
 

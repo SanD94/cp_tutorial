5c6c83e8d4403259b189085dc443275baa883c2c
Safa Andac <8544658+SanD94@users.noreply.github.com>
2024-10-04 11:41:39 +0200

    add intro with editor config

---
+++ intro.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstdio>
+#include<bits/stdc++.h>
+
+// compile command
+// g++ -std=c++11 -O2 -Wall filename.cpp -o filename
+
+// code shortening
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    return 0;
+}
